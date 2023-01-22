de188fb79e8c221cd0795d8dc5927e931e8b4ec6
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-01-22 17:00:10 +0200

    Update task_01_multiplication_function.py

--- 04 Functions Advanced/Lab/task_01_multiplication_function.py
+++ 04 Functions Advanced/Lab/task_01_multiplication_function.py
@@ -1,5 +1,11 @@
-
-
+def multiply(*args):
+    result = 1
+    for item in args:
+        result *= item
+    return result
 
 
 ################################################   Task Description   ################################################
+# 1. Multiplication Function
+# Write a function called multiply that can receive any quantity of numbers (integers) as different parameters
+# and returns the result of the multiplication of all of them. Submit only your function in the Judge system.
