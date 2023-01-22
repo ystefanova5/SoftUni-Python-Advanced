d844497aef0f550b34ad28f7220bd2822f4ff458
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-01-22 17:01:47 +0200

    Update task_02_person_info.py

--- 04 Functions Advanced/Lab/task_02_person_info.py
+++ 04 Functions Advanced/Lab/task_02_person_info.py
@@ -1,5 +1,12 @@
+def get_info(**kwargs):
+    return f"This is {kwargs['name']} from {kwargs['town']} and he is {kwargs['age']} years old"
 
 
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
 
 
 ################################################   Task Description   ################################################
+# 2. Person Info
+# Write a function called get_info that receives a name, an age, and a town and returns a string in the format:
+# "This is {name} from {town} and he is {age} years old". Use dictionary unpacking when testing your function.
+# Submit only the function in the judge system.
