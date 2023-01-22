da5d551629fe97ea12f859f9e710e5ebb0937e7a
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-01-22 17:03:06 +0200

    Update task_05_recursive_power.py

--- 04 Functions Advanced/Lab/task_05_recursive_power.py
+++ 04 Functions Advanced/Lab/task_05_recursive_power.py
@@ -1,5 +1,12 @@
-
-
+def recursive_power(number, power):
+    result = 1
+    if power == 0:
+        return result
+    result = number * recursive_power(number, power - 1)
+    return result
 
 
 ################################################   Task Description   ################################################
+# 5. Recursive Power
+# Create a recursive function called recursive_power() which should receive a number and a power.
+# Using recursion, return the result of number ** power. Submit only the function in the judge system.
