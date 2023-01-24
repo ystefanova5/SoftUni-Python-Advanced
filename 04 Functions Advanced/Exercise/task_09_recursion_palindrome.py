fda05a1f7a45570b65b3df8d917aabc6d05b52aa
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-01-24 09:04:32 +0200

    Update task_09_recursion_palindrome.py

--- 04 Functions Advanced/Exercise/task_09_recursion_palindrome.py
+++ 04 Functions Advanced/Exercise/task_09_recursion_palindrome.py
@@ -1,5 +1,15 @@
-
-
+def palindrome(word, idx):
+    if idx == len(word) // 2:
+        return f"{word} is a palindrome"
+    if word[idx] == word[-idx - 1]:
+        return palindrome(word, idx + 1)
+    else:
+        return f"{word} is not a palindrome"
 
 
 ################################################   Task Description   ################################################
+# 9. Recursion Palindrome
+# Write a recursive function called palindrome() that will receive a word and an index (always 0).
+# Implement the function, so it returns "{word} is a palindrome" if the word is a palindrome
+# and "{word} is not a palindrome" if the word is not a palindrome using recursion.
+# Submit only the function in the judge system.
