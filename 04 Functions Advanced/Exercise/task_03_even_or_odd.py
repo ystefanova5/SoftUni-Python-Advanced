6e0957a4c7def772f9bb56642a52df5cfda60d93
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-01-24 09:01:49 +0200

    Update task_03_even_or_odd.py

--- 04 Functions Advanced/Exercise/task_03_even_or_odd.py
+++ 04 Functions Advanced/Exercise/task_03_even_or_odd.py
@@ -1,5 +1,15 @@
-
-
+def even_odd(*args):
+    *numbers, command = args
+    result = []
+    if command == "even":
+        [result.append(x) for x in numbers if x % 2 == 0]
+    elif command == "odd":
+        [result.append(x) for x in numbers if x % 2 != 0]
+    return result
 
 
 ################################################   Task Description   ################################################
+# 3. Even or Odd
+# Create a function called even_odd() that can receive a different quantity of numbers and a command at the end.
+# The command can be "even" or "odd". Filter the numbers depending on the command and return them in a list.
+# Submit only the function in the judge system.
