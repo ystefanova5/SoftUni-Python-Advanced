aaa2331ce3e68865d61c5749c3c409d1b7af3123
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-02-27 11:30:17 +0200

    Update task_06_function_executor.py

--- 04 Functions Advanced/Exercise/task_06_function_executor.py
+++ 04 Functions Advanced/Exercise/task_06_function_executor.py
@@ -1,8 +1,10 @@
 def func_executor(*args):
     result = ""
+    
     for func in args:
         func_name, func_args = func
         result += f"{func_name.__name__} - {func_name(*func_args)}" + "\n"
+    
     return result
 
 
