b3cb278422acc0cf82c4009e8dcf5564669f07bf
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-02-27 16:12:46 +0200

    Update task_09_recursion_palindrome.py

--- 04 Functions Advanced/Exercise/task_09_recursion_palindrome.py
+++ 04 Functions Advanced/Exercise/task_09_recursion_palindrome.py
@@ -1,8 +1,10 @@
 def palindrome(word, idx):
     if idx == len(word) // 2:
         return f"{word} is a palindrome"
+    
     if word[idx] == word[-idx - 1]:
         return palindrome(word, idx + 1)
+    
     else:
         return f"{word} is not a palindrome"
 
