76b8c67ba60ed317e2fb9228aad867e388d79842
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-02-27 09:44:50 +0200

    Update task_03_even_or_odd.py

--- 04 Functions Advanced/Exercise/task_03_even_or_odd.py
+++ 04 Functions Advanced/Exercise/task_03_even_or_odd.py
@@ -1,11 +1,14 @@
 def even_odd(*args):
     *numbers, command = args
     result = []
-    if command == "even":
+    
+      if command == "even":
         [result.append(x) for x in numbers if x % 2 == 0]
-    elif command == "odd":
+    
+   elif command == "odd":
         [result.append(x) for x in numbers if x % 2 != 0]
-    return result
+    
+   return result
 
 
 ################################################   Task Description   ################################################
