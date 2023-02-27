df5227762a02a2cc6fd8622739be056c1c848949
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-02-27 11:40:38 +0200

    Update task_07_grocery.py

--- 04 Functions Advanced/Exercise/task_07_grocery.py
+++ 04 Functions Advanced/Exercise/task_07_grocery.py
@@ -1,8 +1,11 @@
 def grocery_store(**kwargs):
     receipt = ""
+    
     sorted_products = sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+    
     for key, value in sorted_products:
         receipt += f"{key}: {value}" + "\n"
+    
     return receipt
 
 
