228e1ffa88a88a91d5c27ce2e5027f6e2432844b
Yordanka Stefanova <110626738+ystefanova5@users.noreply.github.com>
2023-02-27 11:42:35 +0200

    Update task_08_age_assignment.py

--- 04 Functions Advanced/Exercise/task_08_age_assignment.py
+++ 04 Functions Advanced/Exercise/task_08_age_assignment.py
@@ -1,12 +1,16 @@
 def age_assignment(*args, **kwargs):
     result = ""
+    
     for full_name in args:
         first_letter = full_name[0]
         kwargs[full_name] = kwargs[first_letter]
         del kwargs[first_letter]
+    
     sorted_names = sorted(kwargs.items(), key=lambda x: x[0])
+    
     for name, age in sorted_names:
         result += f"{name} is {age} years old." + "\n"
+    
     return result
 
 
